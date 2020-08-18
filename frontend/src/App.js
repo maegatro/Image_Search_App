0d62c570a011d38e64d38e64221eb2595afd8f0e
maegatro <shuntaromaekawa@gmail.com>
2020-08-18 16:39:02 +0900

    showing pictures when user search with keyword

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,29 @@
 import React, { useState, useEffect, useRef } from "react";
 import "./App.css";
 import axios from "axios";
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 function App() {
-  const [query, setQuery] = useState("");
+  // const [query, setQuery] = useState("");
   const [photos, setPhotos] = useState([]);
   const [keyword, setKeyword] = useState("");
 
-  useEffect(() => {
-    axios
-      // https://api.unsplash.com/search/photos?page=3&query=office
-      .get(`https://api.unsplash.com/search/photos?page=1&query=${query}`)
-      .then((res) => {
-        const response = res.result;
-        setQuery(response);
-      });
-  }, [query, photos]);
+  const getPhotos = async () => {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: {
+        query: keyword,
+        client_id: `${API_KEY}`,
+      },
+      // headers: {
+      //   Authorization: "2-845ep38EjucmvKrUMPtGw8mudOjho7xSt4qc-KTgM",
+      // },
+    });
+    setPhotos(response.data.results);
+  };
 
-  console.log(query);
+  useEffect(() => {
+    getPhotos();
+  }, [keyword]);
 
   return (
     <div className="box">
@@ -27,20 +33,22 @@ function App() {
           <input
             type="search"
             placeholder="search"
-            onChange={(e) => setQuery(e.target.value)}
+            onChange={(e) => {
+              setKeyword(e.target.value);
+            }}
           />
         </label>
       </form>
-      {/* <ul className="photo-grid">
+      <ul className="photo-grid">
         {photos.map((photo) => {
           return (
             <li key={photo.id}>
-              <img src={`https://source.unsplash.com/?${keyword}`} />
+              <img src={photo.urls.regular} />
             </li>
           );
         })}
-      </ul> */}
-      <img src={`https://source.unsplash.com/300x400/?${keyword}`} />
+      </ul>
+      {/* <img src={`https://source.unsplash.com/300x400/?${keyword}`} /> */}
     </div>
   );
 }
